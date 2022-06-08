e8c48fedaf52854b36da9c2852fae0ce78c8d863
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-08 19:36:26 +0500

    Includes mix path in view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,19 @@
 
 <head>
     <title>{{$pageTitle}}</title>
-    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
     </link>
 </head>
 
 <body>
+    <div class="row">
+        <div class="col-md-6">
 
+        </div>
+        <div class="col-md-6">
+
+        </div>
+    </div>
 </body>
 
 </html>
