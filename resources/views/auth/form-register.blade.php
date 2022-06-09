42b87923f9f84c5d1d994187f3bafbb554eb458b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-09 22:47:42 +0500

    Specifies route in registe rform

--- resources/views/auth/form-register.blade.php
+++ resources/views/auth/form-register.blade.php
@@ -1,4 +1,4 @@
-{{Form::open(['class' => 'form-register'])}}
+{{Form::open(['route' => 'registerUser', 'class' => 'form-register'])}}
 <div class="row dv-base">
     <div class="col-md-4 d-flex justify-content-center">
         <img src="/images/petcare-logo.png" id="img-logo" />
