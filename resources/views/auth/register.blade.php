69c4d1034305d1a77143408a7407152d6137c186
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-09 21:09:38 +0500

    Adds register fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,8 +12,45 @@
         <div class="col-md-6 d-flex justify-content-center">
             <img src="/images/petcare-logo.png" id="img-logo" />
         </div>
+        <div class="col-md-6">
+            <div class="row justify-content-center">
+                {{Form::open(['class' => 'form-login'])}}
+                <div class="col-md-12 mb-3">
+                    {{Form::label('first_name', 'First Name')}}
+                    {{Form::text('first_name', '', ['class' => 'form-control'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('last_name', 'Last Name')}}
+                    {{Form::text('last_name', '', ['class' => 'form-control'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('email', 'Email')}}
+                    {{Form::text('email', '', ['class' => 'form-control'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('phone_no', 'Phone Number')}}
+                    {{Form::text('phone_no', '', ['class' => 'form-control'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('country_id', 'Country')}}
+                    {{Form::select('country_id', [], '', ['class' => 'form-select'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('role', 'Role')}}
+                    {{Form::select('role', [], '', ['class' => 'form-select'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('password', 'Password')}}
+                    {{Form::password('password', ['class' => 'form-control'])}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    {{Form::label('confirm_password', 'Confirm Password')}}
+                    {{Form::password('password', ['class' => 'form-control'])}}
+                </div>
+                {{Form::close()}}
+            </div>
+        </div>
     </div>
-
 </body>
 
 </html>
