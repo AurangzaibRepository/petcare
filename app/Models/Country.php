1b8f1f5a569c4cd1a666dfdea79f6611b94bc82d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-09 23:53:42 +0500

    Implements function to get data for dropdowm

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,7 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Country extends Model
 {
+    public function getAll(): Collection
+    {
+        $data = $this->orderBy('country_name')
+                     ->pluck('country_name', 'id');
+
+        return $data;
+    }
 }
